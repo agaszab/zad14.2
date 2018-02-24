7007acc1a2e64eb98bcc7f9fb31d265120df2797
agaszab <gatito@poczta.pl>
2018-02-24 20:01:28 +0100

    przeliczanie słów, znaków, palindrom

--- src/PrzeliczanieServlet.java
+++ src/PrzeliczanieServlet.java
@@ -12,36 +12,42 @@ import java.util.Arrays;
 @WebServlet("/PrzeliczanieServlet")
 public class PrzeliczanieServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        response.setCharacterEncoding("UTF-8");
+        request.setCharacterEncoding("UTF-8");
         String tekst=request.getParameter("tekst");
-        String[] tab = tekst.split("\\s");
-        int ileSlow=Arrays.toString(tab).length();
+        String[] tab = tekst.split(" ");
+        String wyrazy= Arrays.toString(tab);
+        int ileSlow=tab.length;
 
         String tekstBezSpacji = tekst.replaceAll("\\s", "");
         int bezSpacji=tekstBezSpacji.length();
 
         int ileWszystkich=tekst.length();
-
-        boolean palindrom=czyPalindrom(tab) ;
         PrintWriter writer = response.getWriter();
+        boolean palindrom=czyPalindrom(tekstBezSpacji);
+
+
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+
         writer.println(tekst);
 
         writer.println("Ilość słów:"+ileSlow );
-        writer.println("Ilość znaków:"+bezSpacji );
-        writer.println("Ilość znaków bez spacji:"+ileWszystkich );
+        writer.println("Ilość znaków:"+ileWszystkich );
+        writer.println("Ilość znaków bez spacji:"+bezSpacji );
         writer.println("Palindrom:"+palindrom );
+
     }
 
 
 
-    boolean czyPalindrom(String tab[])
+    boolean czyPalindrom(String tekstBezSpacji)
     {
-    int j=tab.length;
-        for(int i=0; i<=j/2; i++)
-            if(tab[i].equals(tab[j-i]))
-                return false;
-
-        return true;
+        StringBuffer sb=new StringBuffer(tekstBezSpacji);
+        sb.reverse();
+        String revers=sb.toString();
+        if(revers.equals(tekstBezSpacji)) return true;
+        else
+            return false;
     }
-
 }
